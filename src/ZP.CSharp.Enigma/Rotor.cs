dfd65c32f055cc7dd4419686e565df75c2bb8583
Z Panda <zp.zpanda@gmail.com>
2024-08-14 15:48:39 +0800

    feat: add stepping to rotors

--- src/ZP.CSharp.Enigma/Rotor.cs
+++ src/ZP.CSharp.Enigma/Rotor.cs
@@ -11,6 +11,35 @@ namespace ZP.CSharp.Enigma
     */
     public class Rotor<T>
     {
+        private int _Position;
+        /**
+        <summary>The position of this rotor.</summary>
+        */
+        public int Position
+        {
+            get => _Position;
+            set => _Position = value;
+        }
+
+        private int[] _Notch;
+        /**
+        <summary>The notches of this rotor.</summary>
+        */
+        public int[] Notch
+        {
+            get => _Notch;
+            [MemberNotNull(nameof(_Notch))]
+            set
+            {
+                ArgumentNullException.ThrowIfNull(value);
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException("Rotor must have at least one notch.");
+                }
+                _Notch = value;
+            }
+        }
+
         private (T Entrywheel, T Reflector)[] _Pairs;
         /**
         <summary>The pairs in this rotor.</summary>
@@ -27,13 +56,47 @@ namespace ZP.CSharp.Enigma
         }
 
         /**
-        <summary>Creates a rotor with the pairs provided.</summary>
+        <summary>Creates a rotor with the position, notches, and pairs provided.</summary>
+        <param name="pos">The position provided.</param>
+        <param name="notch">The notches provided.</param>
         <param name="pairs">The pairs provided.</param>
-        <returns>A rotor created with the pairs provided.</returns>
+        <returns>A rotor created with the position, notches, and pairs provided.</returns>
         */
-        public Rotor(params (T Entrywheel, T Reflector)[] pairs)
+        public Rotor(int pos, int[] notch, params (T Entrywheel, T Reflector)[] pairs)
         {
+            Position = pos;
+            Notch = notch;
             Pairs = pairs;
+        }
+
+        /**
+        <summary>The domain of this rotor.</summary>
+        <para>TODO: Allow domain other than entrywheel order</para>
+        */
+        private T[] Domain => Pairs.Select(p => p.Entrywheel).ToArray();
+
+        /**
+        <summary>Shifts a datum entering the rotor according to this rotor's position.</summary>
+        <param name="data">The datum to be shifted.</param>
+        <returns>The shifted datum.</returns>
+        */
+        private T ShiftIn(T data)
+        {
+            var idx = Array.IndexOf(Domain, data);
+            var len = Domain.Length;
+            return Domain[(idx + Position) % len];
+        }
+
+        /**
+        <summary>Shifts a datum leaving the rotor according to this rotor's position.</summary>
+        <param name="data">The datum to be shifted.</param>
+        <returns>The shifted datum.</returns>
+        */
+        private T ShiftOut(T data)
+        {
+            var idx = Array.IndexOf(Domain, data);
+            var len = Domain.Length;
+            return Domain[(idx - Position + len) % len];
         }
 
         /**
@@ -41,13 +104,24 @@ namespace ZP.CSharp.Enigma
         <param name="data">The datum to map.</param>
         <returns>The mapped datum.</returns>
         */
-        public T FromEntrywheel(T data) => Pairs.Where(p => data.Equals(p.Entrywheel)).Single().Reflector;
+        public T FromEntrywheel(T data) => ShiftOut(Pairs.Where(p => ShiftIn(data).Equals(p.Entrywheel)).Single().Reflector);
 
         /**
         <summary>Maps a datum coming from the reflector.</summary>
         <param name="data">The datum to map.</param>
         <returns>The mapped datum.</returns>
         */
-        public T FromReflector(T data) => Pairs.Where(p => data.Equals(p.Reflector)).Single().Entrywheel;
+        public T FromReflector(T data) => ShiftOut(Pairs.Where(p => ShiftIn(data).Equals(p.Reflector)).Single().Entrywheel);
+
+        /**
+        <summary>Determines whether the next rotor in line is allowed to step.</summary>
+        <returns><see langword="true" /> if allowed, or <see langword="false" /> if not.</returns>
+        */
+        public bool AllowNextToStep() => Notch.Contains(Position);
+
+        /**
+        <summary>Steps the rotor.</summary>
+        */
+        public void Step() => Position = (Position + 1) % Pairs.Length;
     }
 }
